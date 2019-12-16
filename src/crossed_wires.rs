3e7d7baa02419e8ef65aaab6f09a209fd2af431c
Owen Nelson <onelson@gmail.com>
2019-12-15 20:45:40 -0800

    NOT passing day 3 part 2.

--- src/crossed_wires.rs
+++ src/crossed_wires.rs
@@ -1,5 +1,6 @@
-//! --- Day 3 part 1: Crossed Wires ---
+//! --- Day 3: Crossed Wires ---
 //!
+//! --- Part One ---
 //! The gravity assist was successful, and you're well on your way to the Venus
 //! refuelling station. During the rush back on Earth, the fuel management system
 //! wasn't completely installed, so that's next on the priority list.
@@ -59,6 +60,50 @@
 //!   U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
 //!
 //! What is the Manhattan distance from the central port to the closest intersection?
+//!
+//! --- Part Two ---
+//! It turns out that this circuit is very timing-sensitive; you actually need
+//! to minimize the signal delay.
+//!
+//! To do this, calculate the number of steps each wire takes to reach each
+//! intersection; choose the intersection where the sum of both wires' steps is
+//! lowest.
+//! If a wire visits a position on the grid multiple times, use the steps value
+//! from the first time it visits that position when calculating the total value
+//! of a specific intersection.
+//!
+//! The number of steps a wire takes is the total number of grid squares the
+//! wire has entered to get to that location, including the intersection being
+//! considered. Again consider the example from above:
+//!
+//! ```text
+//! ...........
+//! .+-----+...
+//! .|.....|...
+//! .|..+--X-+.
+//! .|..|..|.|.
+//! .|.-X--+.|.
+//! .|..|....|.
+//! .|.......|.
+//! .o-------+.
+//! ...........
+//! ```
+//!
+//! In the above example, the intersection closest to the central port is reached
+//! after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the
+//! second wire for a total of 20+20 = 40 steps.
+//!
+//! However, the top-right intersection is better: the first wire takes only
+//! 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+//!
+//! Here are the best steps for the extra examples from above:
+//!
+//! - R75,D30,R83,U83,L12,D49,R71,U7,L72
+//!   U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+//! - R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+//!   U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+//!
+//! What is the fewest combined steps the wires must take to reach an intersection?
 
 use std::collections::HashSet;
 use std::iter::IntoIterator;
@@ -97,8 +142,14 @@ impl FromStr for Step {
 }
 
 pub fn calc_distance(a: &[Step], b: &[Step]) -> i32 {
-    let a: HashSet<_> = plot_course(a).into_iter().collect();
-    let b: HashSet<_> = plot_course(b).into_iter().collect();
+    let a: HashSet<_> = plot_course(a)
+        .into_iter()
+        .flat_map(|xs| xs.into_iter())
+        .collect();
+    let b: HashSet<_> = plot_course(b)
+        .into_iter()
+        .flat_map(|xs| xs.into_iter())
+        .collect();
 
     a.intersection(&b)
         .into_iter()
@@ -107,16 +158,81 @@ pub fn calc_distance(a: &[Step], b: &[Step]) -> i32 {
         .expect("min distance")
 }
 
-fn plot_course(directions: &[Step]) -> Vec<Point> {
-    let mut buf: Vec<Point> = vec![];
+#[derive(Clone, Default)]
+struct PathWalk {
+    step_count: usize,
+    points: Vec<Vec<Point>>,
+    visited: HashSet<Point>,
+}
+
+impl PathWalk {
+    pub fn new(points: Vec<Vec<Point>>) -> Self {
+        Self {
+            points,
+            ..Default::default()
+        }
+    }
+
+    pub fn crosses(&self, other: &PathWalk) -> bool {
+        other.visited.intersection(&self.visited).count() > 0
+    }
+
+    // Creates a new PathWalk which moves the next pending step into the
+    // visited set.
+    pub fn next(&self) -> Option<PathWalk> {
+        self.points.split_first().map(|(head, tail)| {
+            let mut visited = self.visited.clone();
+            visited.extend(head);
+            let step_count = self.step_count + head.len();
+            Self {
+                points: tail.to_vec(),
+                step_count,
+                visited,
+            }
+        })
+    }
+}
+
+fn walk(a: PathWalk, b: PathWalk) -> Option<usize> {
+    if a.crosses(&b) {
+        return Some(a.step_count + b.step_count);
+    }
+    match (a.next(), b.next()) {
+        (None, None) => None,
+        (Some(next_a), None) => walk(next_a, b),
+        (None, Some(next_b)) => walk(a, next_b),
+        (Some(next_a), Some(next_b)) => {
+            let walked_a = walk(a, next_b);
+            let walked_b = walk(b, next_a);
+            [walked_a, walked_b].into_iter().filter_map(|x| *x).min()
+        }
+    }
+}
+
+pub fn calc_fewest_steps(a: &[Step], b: &[Step]) -> Option<usize> {
+    let walker_a = PathWalk::new(plot_course(a));
+    let walker_b = PathWalk::new(plot_course(b));
+    walk(walker_a, walker_b)
+}
+
+fn plot_course(directions: &[Step]) -> Vec<Vec<Point>> {
+    let mut buf: Vec<Vec<Point>> = vec![];
     for step in directions {
-        let &(x, y) = buf.iter().last().unwrap_or(&(0, 0));
+        // Eh, this is meant to be the last item in the last sub-vec, or `(0, 0)`
+        // if we're just starting from an empty `buf`.
+        let &(x, y) = buf
+            .iter()
+            .last()
+            .and_then(|x| x.iter().last())
+            .unwrap_or(&(0, 0));
 
         match step {
-            &Step::Left(dist) => buf.extend((1..=dist as usize).map(|i| (x - i as i32, y))),
-            &Step::Right(dist) => buf.extend((1..=dist as usize).map(|i| (x + i as i32, y))),
-            &Step::Up(dist) => buf.extend((1..=dist as usize).map(|i| (x, y + i as i32))),
-            &Step::Down(dist) => buf.extend((1..=dist as usize).map(|i| (x, y - i as i32))),
+            &Step::Left(dist) => buf.push((1..=dist as usize).map(|i| (x - i as i32, y)).collect()),
+            &Step::Right(dist) => {
+                buf.push((1..=dist as usize).map(|i| (x + i as i32, y)).collect())
+            }
+            &Step::Up(dist) => buf.push((1..=dist as usize).map(|i| (x, y + i as i32)).collect()),
+            &Step::Down(dist) => buf.push((1..=dist as usize).map(|i| (x, y - i as i32)).collect()),
         };
     }
     buf
@@ -149,19 +265,22 @@ mod day03_1_test {
 
     #[test]
     fn test_plot_course() {
-        assert_eq!(plot_course(&[Step::Right(2)]).as_slice(), &[(1, 0), (2, 0)],);
-        assert_eq!(plot_course(&[Step::Down(1)]).as_slice(), &[(0, -1)],);
+        assert_eq!(
+            plot_course(&[Step::Right(2)]).as_slice(),
+            &[vec![(1, 0), (2, 0)]],
+        );
+        assert_eq!(plot_course(&[Step::Down(1)]).as_slice(), &[vec![(0, -1)]],);
         assert_eq!(
             plot_course(&[Step::Down(1), Step::Right(2)]).as_slice(),
-            &[(0, -1), (1, -1), (2, -1)],
+            &[vec![(0, -1),], vec![(1, -1), (2, -1)]],
         );
         assert_eq!(
             plot_course(&[Step::Left(2), Step::Up(3)]).as_slice(),
-            &[(-1, 0), (-2, 0), (-2, 1), (-2, 2), (-2, 3)],
+            &[vec![(-1, 0), (-2, 0),], vec![(-2, 1), (-2, 2), (-2, 3)]],
         );
         assert_eq!(
             plot_course(&[Step::Up(2), Step::Down(1)]).as_slice(),
-            &[(0, 1), (0, 2), (0, 1)],
+            &[vec![(0, 1), (0, 2),], vec![(0, 1)]],
         );
     }
 
@@ -171,10 +290,32 @@ mod day03_1_test {
         let input_b = build_steps("U62,R66,U55,R34,D71,R55,D58,R83");
         assert_eq!(calc_distance(&input_a, &input_b), 159);
     }
+
     #[test]
     fn test_example_2() {
         let input_a = build_steps("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
         let input_b = build_steps("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
         assert_eq!(calc_distance(&input_a, &input_b), 135);
+    }
+}
+
+#[cfg(test)]
+mod day03_2_test {
+    use super::{build_steps, calc_fewest_steps};
+
+    #[ignore]
+    #[test]
+    fn test_example_1() {
+        let input_a = build_steps("R75,D30,R83,U83,L12,D49,R71,U7,L72");
+        let input_b = build_steps("U62,R66,U55,R34,D71,R55,D58,R83");
+        assert_eq!(calc_fewest_steps(&input_a, &input_b), Some(610));
+    }
+
+    #[ignore]
+    #[test]
+    fn test_example_2() {
+        let input_a = build_steps("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51");
+        let input_b = build_steps("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7");
+        assert_eq!(calc_fewest_steps(&input_a, &input_b), Some(410));
     }
 }
