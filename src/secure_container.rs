765d8759e0f69e66958fbfdde833b69ce248f056
Owen Nelson <onelson@gmail.com>
2019-12-19 00:35:18 -0800

    Pass for day 4 part 2. Refactored some common stuff out too.

--- src/secure_container.rs
+++ src/secure_container.rs
@@ -22,26 +22,77 @@
 //!
 //! How many different passwords within the range given in your puzzle input
 //! meet these criteria?
+//!
+//! ## Part 2
+//!
+//! An Elf just remembered one more important detail: the two adjacent matching
+//! digits are not part of a larger group of matching digits.
+//!
+//! Given this additional criterion, but still ignoring the range rule, the
+//! following are now true:
+//!
+//! - `112233` meets these criteria because the digits never decrease and all
+//!   repeated digits are exactly two digits long.
+//! - `123`**`444`** no longer meets the criteria (the repeated `44` is part of a larger
+//!   group of `444`).
+//! - `111122` meets the criteria (even though 1 is repeated more than twice, it
+//!   still contains a double `22`).
+//!
+//! How many different passwords within the range given in your puzzle input
+//! meet all of the criteria?
 
-/// Checks to see if a number conforms to the password policy.
-pub fn is_valid(n: u32) -> bool {
-    let digits = to_digits(n);
+// validation rules common to parts 1 and 2.
+fn common_validation(digits: &[u32]) -> bool {
     if digits.len() != 6 {
         return false;
     }
 
-    let mut sorted = digits.clone();
+    let mut sorted = digits.to_vec();
     sorted.sort();
-    if sorted != digits {
+
+    if digits != sorted.as_slice() {
         return false;
     }
+    return true;
+}
 
+/// Checks to see if a number conforms to the password policy outlined in part 1.
+pub fn is_valid(n: u32) -> bool {
+    let digits = to_digits(n);
+    if !common_validation(&digits) {
+        return false;
+    }
     let mut prev = -1;
     for i in &digits {
         if prev == *i as i32 {
             return true;
         }
         prev = *i as i32;
+    }
+
+    return false;
+}
+
+/// Checks to see if a number conforms to the password policy outlined in part 2.
+pub fn is_more_valid(n: u32) -> bool {
+    let digits = to_digits(n);
+
+    if !common_validation(&digits) {
+        return false;
+    }
+
+    let mut distinct = digits.clone();
+    distinct.dedup();
+
+    for i in distinct {
+        let count = digits
+            .iter()
+            .skip_while(|x| **x != i)
+            .take_while(|x| **x == i)
+            .count();
+        if count == 2 {
+            return true;
+        }
     }
 
     return false;
@@ -57,7 +108,7 @@ fn to_digits(n: u32) -> Vec<u32> {
 
 #[cfg(test)]
 mod day04_1_tests {
-    use super::*;
+    use super::{is_valid, to_digits};
 
     #[test]
     fn test_to_digits() {
@@ -78,5 +129,25 @@ mod day04_1_tests {
     #[test]
     fn test_123789_is_not_valid() {
         assert!(!is_valid(123789));
+    }
+}
+
+#[cfg(test)]
+mod day04_2_tests {
+    use super::is_more_valid;
+
+    #[test]
+    fn test_112233_is_valid() {
+        assert!(is_more_valid(112233));
+    }
+
+    #[test]
+    fn test_123444_is_not_valid() {
+        assert!(!is_more_valid(123444));
+    }
+
+    #[test]
+    fn test_111122_is_valid() {
+        assert!(is_more_valid(111122));
     }
 }
